b4663ce105e269e99c297324372c17975bea7ee1
pillisrihariB8 <sriharipilli85@gmail.com>
2025-10-14 10:29:43 +0530

    Add files via upload

---
+++ shopping cart.py
@@ -0,0 +1,52 @@
+class ShoppingCart:
+    def __init__(self):
+        self.items=[]
+
+    def add_item(self,item_name,qty):
+        item=(item_name,qty)
+        self.items.append(item)
+
+    def remove_item(self,item_name):
+
+        for item in self.items:
+            if item[0]==item_name:
+                self.items.remove(item)
+                break
+
+    def calculate_total(self):
+      total=0
+      for item in self.items:
+        total+=item[1]
+        return total
+#driver code
+cart = ShoppingCart()
+#Add items to the shopping Cart
+cart.add_item("Banana",250)
+cart.add_item("Mango",100)
+cart.add_item("Orange",50)
+
+print("Current Items in Cart:")
+for item in cart.items:
+    print(item[0],"-",item[1])
+
+total_qty=cart.calculate_total()
+print("Total Quantity:",total_qty)
+
+cart.remove_item("Orange")
+print("\nUpdated Items in Cart after removing in Orange:")
+for item in cart.items:
+    print(item[0],"-",item[1])
+total_qty=cart.calculate_total()
+print("Total Quantity",total_qty)
+
+
+
+
+
+
+
+
+
+
+
+
