91f4252a3dd5d94c1aba610bb0f694d6a3290c01
pillisrihariB8 <sriharipilli85@gmail.com>
2025-10-14 10:57:01 +0530

    Add files via upload

---
+++ bike rental.py
@@ -0,0 +1,39 @@
+# Bike Rental System
+
+class BikeShop:
+
+    def __init__(self,stock):
+        self.stock=stock
+
+    def ShowBike(self):
+        print("Total Bikes:",self.stock)
+
+    def rentBike(self,quantity):
+
+     if quantity<=0:
+      print("Please Enter the Positive Value or greater than Zero")
+     elif quantity.self.stock:
+             print("Enter the Value(less than stock)")
+     else:
+        self.stock=self.stock-quantity
+        print("Total Price:",quantity * 100)
+        print("Total Bikes:",self.stock)
+
+while True:
+    obj=BikeShop(100)
+    user_input=int(input('''
+    1->Display Stock
+    2->Rent a Bike
+    3->Exit
+               '''))
+    if user_input==1:
+       obj.ShowBike()
+    elif user_input==2:
+        n=int(input("Enter The Rent Bike:->"))
+        obj.rentBike(n)
+    else:
+        break
+
+
+
+        
